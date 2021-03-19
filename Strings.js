201b8fa4561489fdfb12177ea8fa748168607274
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 09:20:45 +0700

    Create String.js and add detail

---
+++ Strings.js
@@ -0,0 +1,18 @@
+// string
+console.log("Hello, World");
+
+let email = "casnova152@gmail.com"
+console.log(email);
+
+// string concatenation
+let firstName = "Kongsittha";
+let lastName = "Nimking";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// getting chatacters
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length);
