e252bc0bf6d605d3d50963f0df5f62f540a145fe
Kongsittha Nimking <casnova152@gmail.com>
2021-03-19 17:39:36 +0700

    Add files

---
+++ Other Ways to Query the DOM/sandbox.js
@@ -0,0 +1,13 @@
+// get an elements by ID
+const title = document.getElementById("page-title");
+console.log(title);
+
+// get Elements by their class name
+const errors = document.getElementsByClassName("error");
+console.log(errors);
+console.log(errors[0]);
+
+// get elements by their tag name
+const paras = document.getElementsByTagName("p");
+console.log(paras);
+console.log(paras[0]);
