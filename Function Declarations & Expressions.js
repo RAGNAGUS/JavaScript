722da12022a5f7165bbbc138bd2503d4cdaf168f
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 14:48:59 +0700

    Create Function Declarations & Expressions.js

---
+++ Function Declarations & Expressions.js
@@ -0,0 +1,24 @@
+// function declaration
+function greet() {
+    console.log("hello there");
+}
+
+//function expression
+const speak = function() {
+    console.log("good day!")
+};
+
+greet();
+speak();
+greet2();
+speak2();
+
+
+function greet2() {
+    console.log("hello there");
+}
+
+// this is not working because Hoisting
+const speak2 = function() {
+    console.log("good day!")
+};
