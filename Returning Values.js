82253ead6b1d7023b8e975b6e152f261ee081ce6
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 15:00:57 +0700

    Create Returning Values.js

---
+++ Returning Values.js
@@ -0,0 +1,17 @@
+// arguments & parameters
+
+// const speak = function(name = "Luigi", time = "night") {
+//     console.log(`good ${time} ${name}`);
+// };
+
+const calcArea = function(radius) {
+    return 3.14 * radius ** 2;
+}
+
+const calcVol = function(area) {
+    console.log(area);
+}
+
+const area = calcArea(5);
+
+calcVol(area);
