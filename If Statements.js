5eaed77e91de89fd284f016eaa96c778f25fad9e
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 11:47:51 +0700

    Create If Statements.js

---
+++ If Statements.js
@@ -0,0 +1,19 @@
+// if statements
+
+const age = 25;
+
+if (age > 20) {
+    console.log("you are over 20 years old");
+}
+
+const ninjas = ["Shaun", "Ryu", "Chun-Li", "Yoshi"]
+
+if (ninjas.length > 5) {
+    console.log("That's a lot of ninjas");
+}
+
+const password = "password";
+
+if (password.length >= 8) {
+    console.log("That password is long enough!");
+}
