2bfb6cc6f7e85578c0ceb42052292f0c140c78de
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 16:52:28 +0700

    Create Math Object.js

---
+++ Math Object.js
@@ -0,0 +1,19 @@
+// Math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 5.8;
+
+console.log(Math.round(area)); // return 6
+console.log(Math.floor(area)); // return 5
+console.log(Math.ceil(area)); // return 6
+console.log(Math.trunc(area)); // return 5
+
+// random numbers
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 100));
