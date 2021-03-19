35b2a8c191bbd04a083d9a8ce985106689693859
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 15:51:20 +0700

    Create Callback Functions in Action.js

---
+++ Callback Functions in Action.js
@@ -0,0 +1,13 @@
+// get a reference to the 'ul'
+const ul = document.querySelector('.people');
+
+const people = ["Mario", "Luigi", "Ryu", "Shaun", "Chun-Li"];
+
+let html = ``;
+
+people.forEach((person) => {
+    html += `<li style="color: purple">${person}</li>`;
+});
+
+console.log(html);
+ul.innerHTML = html;
