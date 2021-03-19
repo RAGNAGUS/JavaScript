42af6e552efdb011a0b2f2fbfaf7b4ce9c26e893
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 15:30:57 +0700

    Create Functions vs Methods.js

---
+++ Functions vs Methods.js
@@ -0,0 +1,12 @@
+const user = "Shaun";
+
+// functions
+const greet = () => "hello";
+
+let resultOne = greet();
+console.log(resultOne);
+
+
+// methods
+let resultTwo = user.toUpperCase();
+console.log(resultTwo);
