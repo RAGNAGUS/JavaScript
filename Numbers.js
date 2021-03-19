1271ce8a95efaa83425932465eb85e0ff4c2080b
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 09:45:36 +0700

    Create Numbers.js

---
+++ Numbers.js
@@ -0,0 +1,39 @@
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius);
+
+//math operators +, -, *, /, **, %
+
+console.log(10 / 2);
+let result = radius % 3;
+
+let result = pi * radius ** 2;
+
+// oder of operation - B I T M A S stands for Brackets, Indices, Division, Multiplication, Addition, Subtraction.
+
+let result = 5 * (10 - 3) ** 2;
+
+console.log(result);
+
+let likes = 10;
+
+likes = likes + 1;
+
+likes++;
+likes--;
+
+likes += 10;
+likes -= 5;
+likes *= 2;
+likes /= 2;
+
+console.log(likes);
+
+// NaN - not a number
+console.log(5 / "Hello");
+console.log(5 * "Hello")
+
+// concatenate number
+let result = "The blog has " + likes + " likes ";
+console.log(result);
