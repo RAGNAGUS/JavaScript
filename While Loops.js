8c46f37ce773d69e3b586f24513850edac2ebf02
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 11:39:42 +0700

    Create While Loops.js

---
+++ While Loops.js
@@ -0,0 +1,16 @@
+// while loops
+
+const names = ["Shaun", "Mario", "Luigi"]
+
+let i = 0;
+
+while (i < 5) {
+    console.log("in loop: ", i);
+    i++;
+}
+
+let i = 0;
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
+}
