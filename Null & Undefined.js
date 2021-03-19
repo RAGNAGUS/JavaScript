6b84467d56303ea96b01e6e18580e21ecbf27717
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 10:38:28 +0700

    Create Null & Undefined.js

---
+++ Null & Undefined.js
@@ -0,0 +1,11 @@
+let age;
+
+console.log(age); //undefined
+console.log(age + 3); //NaN
+console.log(`"The age is ${age}"`) //"The age is undefined"
+
+let year = null;
+
+console.log(year); //null
+console.log(year + 3); //3
+console.log(`"The age is ${year}"`) //"The age is null"
