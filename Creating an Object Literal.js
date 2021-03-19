473cd56cb49ba0ef4e344434732f977890cab8a7
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 16:05:13 +0700

    Create Creating an Object Literal.js

---
+++ Creating an Object Literal.js
@@ -0,0 +1,21 @@
+// object literals
+
+let user = {
+    name: "RAGNAGUS",
+    age: 30,
+    email: "casnova152@gmail.com",
+    location: "Thailand",
+    blog: ["Why mac & cheese rules", "10 things to make with marmite"],
+};
+
+console.log(user);
+console.log(user.name);
+
+user.age = 35;
+console.log(user.age);
+
+console.log(user["location"]);
+user["name"] = "Chun-Li";
+console.log(user["name"]);
+
+console.log(typeof user);
