f86a929cd4c4b4a548abfc3349c9cd92f5d14fdf
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-03-19 11:58:26 +0700

    Create Logical Operators.js

---
+++ Logical Operators.js
@@ -0,0 +1,11 @@
+// if statements
+
+const password = "";
+
+if (password.length >= 12 && password.includes("@")) {
+    console.log("That password is might strong!")
+} else if (password.length >= 8 || password.includes("@") && password.length > 5) {
+    console.log("That password is strong enough!");
+} else {
+    console.log("That password is too weak!")
+}
