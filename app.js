f449385e0b0b970956405282a21143185619bcc9
Kongsittha Nimking <casnova152@gmail.com>
2021-03-20 19:23:11 +0700

    Add Project files

---
+++ app.js
@@ -0,0 +1,48 @@
+const corectAnswer = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form')
+const result = document.querySelector('.result')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    //check answer
+    userAnswers.forEach((answer, index) => {
+        if (answer == corectAnswer[index]) {
+            score += 25;
+        }
+    });
+
+    //show result on page
+    scrollTo(0, 0);
+    result.classList.remove('d-none');
+
+    // show score with animate
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`
+        if (output == score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+
+});
+
+// window object (global object)
+
+// console.log('hello');
+// window.console.log('hello');
+
+// console.log(document.querySelector('form'));
+// console.log(window.document.querySelector('form'));
+
+// alert('hello');
+// window.alert('hello');
+
+// setTimeout(() => {
+//     alert('hello');
+// }, 3000);
