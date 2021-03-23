17144cfeb32a7bca8dccd7bb87f752d3ea8c617a
Kongsittha Nimking <casnova152@gmail.com>
2021-03-23 12:49:48 +0700

    add Local Storage to Weather App project

---
+++ Updating the Weather App with Local Storage/scripts/forecast.js
@@ -0,0 +1,23 @@
+const key = 'need to create an app from https://developer.accuweather.com/ and take API to here';
+
+//get city information
+const getCity = async(city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+//get weather information
+const getWeather = async(id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
